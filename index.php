8f2b492355ed4f2f067a7df1e335f00575ceb14f
SSJNM <64457549+SSJNM@users.noreply.github.com>
2020-07-06 14:04:31 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<body bgcolor="orange">
+  This is the php code from developer side and it works inside docker container
+  
+  <pre>
+  <?php
+		print `ifconfig`;
+	?>	
+  </pre>
+</html>
