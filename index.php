daf83cdbd52b4d1d6453938946e6cc78104bd284
SSJNM <nishant010singh@gmail.com>
2020-07-17 15:47:55 +0530

    latest update

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
-	<body bgcolor="orange">
+  <body bgcolor="orange">
     Here is the latest update of php code by our developer
     And yes my job for devops CI/CD is done
-  
-  <pre>
-  <?php
+    <pre>
+      <?php
 		print `ifconfig`;
-  ?>	
-  </pre>
+      ?>	
+    </pre>
+  </body>
 </html>
